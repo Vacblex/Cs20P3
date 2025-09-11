702cc1e6b0bd9214b2490bdc3348331413eee1cd
49328004 <49328004@L815WD220029.edu.cbe.ab.ca>
2025-09-11 09:18:28 -0600

    9:18 am

--- Chapter3/src/skillbuilders/RectanglePerimeter.java
+++ Chapter3/src/skillbuilders/RectanglePerimeter.java
@@ -1,12 +1,22 @@
 package skillbuilders;
+import java.util.Scanner;
 
 public class RectanglePerimeter {
 
 	public static void main(String[] args) {
 		
+		int length = 0;
+		int width = 0;
+		Scanner scanner = new Scanner(System.in);
 		
-		//
+		int input = scanner.nextInt();
+		System.out.println("What is the length of the rectangle");
 		
+		int input2 = scanner.nextInt();
+		System.out.println("What is the width of the rectangle");
+		
+		int answer = input + input2;
+		System.out.println(answer);
 	}
 
 }
