51fab534d788b1bd2d215b9c29e441fb70031038
49328004 <49328004@L815WD220029.edu.cbe.ab.ca>
2025-09-15 11:15:08 -0600

    11:15 am

---
+++ Chapter3/src/mastery/Exercise7.java
@@ -0,0 +1,13 @@
+package mastery;
+
+public class Exercise7 {
+	public static void main(String[] args) { 
+		System.out.println("    _________");
+		System.out.println("   |         |");
+		System.out.println("  |   |   |   |");
+		System.out.println(" |      >      |");
+		System.out.println(" |   *     *   |");
+		System.out.println("  |   *****   |");
+		System.out.println("   |_________|");
+	}
+}
