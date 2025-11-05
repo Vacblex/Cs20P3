0a417c212bfa4b7b45d5f0c67f22df484bc45233
49328004 <49328004@L815WD220029.edu.cbe.ab.ca>
2025-11-05 10:46:17 -0700

    10:45 AM

---
+++ Chapter6/src/mastery/Exercise1.java
@@ -0,0 +1,66 @@
+package mastery;
+
+/*
+Thomas Ly
+House maker, 3 different methods.
+10:45AM 11/5/2025
+*/
+
+public class Exercise1 {
+	public static void main(String[] args) {
+        addRoof();
+        addBase();
+        addWalk();
+    }
+    
+    private static void addRoof() {
+        System.out.println("               ( )");
+        System.out.println("      ()      (   )      ()");
+        System.out.println("     (  )    (     )    (  )");
+        System.out.println("    (    )  (       )  (    )");
+        System.out.println("   (      )(         )(      )");
+        System.out.println("  (       (           )       )");
+        System.out.println(" (_______(             )_______)");
+        System.out.println(" |      (_______________)      |");
+    }
+    
+    private static void addBase() {
+        System.out.println(" |      |     _____     |      |");
+        System.out.println(" |      |    |__|__|    |      |");
+        System.out.println(" |  ____|    |__|__|    |____  |");
+        System.out.println(" | |__|_|   _________   |_|__| |");
+        System.out.println(" | |__|_|  |    |    |  |_|__| |");
+        System.out.println(" |      |  |    |    |  |      |");
+        System.out.println(" |______|  |   .|.   |  |______|");
+        System.out.println("        |__|____|____|__|");
+    }
+    
+    private static void addWalk() {
+        System.out.println("               | |");
+        System.out.println("               | |___________________________");
+        System.out.println("               |_____________________________");
+    }
+}
+
+/* Screen dump
+               ( )
+      ()      (   )      ()
+     (  )    (     )    (  )
+    (    )  (       )  (    )
+   (      )(         )(      )
+  (       (           )       )
+ (_______(             )_______)
+ |      (_______________)      |
+ |      |     _____     |      |
+ |      |    |__|__|    |      |
+ |  ____|    |__|__|    |____  |
+ | |__|_|   _________   |_|__| |
+ | |__|_|  |    |    |  |_|__| |
+ |      |  |    |    |  |      |
+ |______|  |   .|.   |  |______|
+        |__|____|____|__|
+               | |
+               | |___________________________
+               |_____________________________
+*/
+
